b11ea709b262e07ddf673600a01cfbd2d4ac6eb7
rafail-r <rafail.re@gmail.com>
2016-04-27 18:47:57 +0300

    paginator and some css

--- web-app/searchapp/apps.py
+++ web-app/searchapp/apps.py
@@ -7,6 +7,7 @@ from elasticsearch import Elasticsearch
 # Establish Connection with Elasticsearch
 elastic_client = Elasticsearch()
 index_name = 'elasticplaces'
+max_size = 50
 
 # Establish Connection with MongoDB
 mongo_client = MongoClient()['test_db']['books']
--- web-app/searchapp/views.py
+++ web-app/searchapp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 import json, requests
 from bson.objectid import ObjectId
-from apps import mongo_client, elastic_client, index_name
+from apps import mongo_client, elastic_client, index_name, max_size
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # homepage
@@ -26,14 +27,24 @@ def search_results(request):
         }
     }
     results = []
-    res = elastic_client.search(index=index_name, body=search_query)
+    res = elastic_client.search(index=index_name, body=search_query, size=max_size)
     for item in res['hits']['hits']:
         temp = {}
         temp['name'] = item['_source']['name']
         temp['formatted_address'] = item['_source']['formatted_address']
         temp['id'] = item['_id']
         results.append(temp)
-    return render(request, "searchapp/results.html", {'res' : results})
+    paginator = Paginator(results, 15) # Show 15 results per page
+    page = request.GET.get('page')
+    try:
+        page_results = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        page_results = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        page_results = paginator.page(paginator.num_pages)
+    return render(request, "searchapp/results.html", {'res' : page_results, 'search_key' : search_key})
 
 # query elasticsearch with id, display at details.html
 def get_by_id(request, _id):
@@ -58,7 +69,7 @@ def live_search(request):
                 }
             }
         }
-        res = elastic_client.search(index=index_name, body=search_query)
+        res = elastic_client.search(index=index_name, body=search_query, size=5)
         for item in res['hits']['hits']:
             temp = {}
             temp['name'] = item['_source']['name']
@@ -66,3 +77,4 @@ def live_search(request):
             temp['id'] = item['_id']
             live_results.append(temp)
     return render(request, 'searchapp/ajax_search.html', {'results' : live_results})
+

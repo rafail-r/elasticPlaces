d5a6e94406209886587cca2fc3cd4e4fea8c9382
rafail <rafail.re@gmail.com>
2016-10-18 01:53:48 +0300

    transporter config

--- elasticsearch examples/transporter config/transformer.js
+++ elasticsearch examples/transporter config/transformer.js
@@ -1,8 +1,12 @@
 module.exports = function(doc) {
   var new_data = {};
   new_data["_id"] = doc["data"]["_id"]["$oid"];
-  new_data["firstName"] = doc["data"]["firstName"];
-  doc["data"] = new_data;
+  new_data["geometry"] = doc["data"]["geometry"];
+  new_data["name"] = doc["data"]["name"];
+  new_data["formatted_address"] = doc["data"]["formatted_address"];
+  new_data["rating"] = doc["data"]["rating"];
+  new_data["types"] = doc["data"]["types"];
+  doc["data"] = new_data
   //console.log("transformer: " + JSON.stringify(doc));
   return doc
 }
